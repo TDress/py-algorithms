8311a8dbac6c56287a178fd781f29273fc4a733e
Thomas Dressler <Thomas.Dressler1@gmail.com>
2017-12-28 16:23:43 -0700

    fix merge_sort algorithm to perform in-place

--- sorting/merge-sort.py
+++ sorting/merge-sort.py
@@ -1,26 +1,52 @@
+from lib import assertion
 
-
-def merge(num_list_1, num_list_2):
-    if len(num_list_1) == 0:
-        return num_list_2
-    elif len(num_list_2) == 0:
-        return num_list_1
+def merge(l1, l2, l):
+    """Merge sorted lists l1 and l2 together so that the answer is sorted.
+    Overwrite l with the answer-- everything is done in-place"""
+    l1_i = l2_i = 0
     
+    while l1_i + l2_i < len(l):
+        if l1_i == len(l1) or (l2_i < len(l2) and l2[l2_i] < l1[l1_i]):
+            l[l1_i + l2_i] = l2[l2_i]
+            l2_i += 1
+        else:
+            l[l1_i + l2_i] = l1[l1_i]
+            l1_i += 1
+            
 
+def merge_sort(l):
+    len_l = len(l)
+    if len_l > 1:
+        mid = len_l // 2
+        l1 = l[:mid]
+        l2 = l[mid:]
 
+        merge_sort(l1)
+        merge_sort(l2)
 
-def merge_sort(numbers):
-    list_length = len(numbers)
-    if list_length == 0:
-        raise RuntimeError('A non-empty list must be provided')
-    elif list_length == 1 and type list_length != int:
-        raise TypeError('All values in the list must be integers!')
-    elif list_length == 1:
-        return numbers
+        merge(l1, l2, l)
 
-    right_start = list_length // 2
-    right = numbers[right_start:]
-    left = numbers[:right_start]
+def main():
+    l1 = [3,2,5,4,1]
+    l2 = [1,2,3]
+    l3 = [3]
+    l4 = [777,11,1,8,1234,56,2,4]
+    l5 = [6,2]
+    l6 = []
 
-    return merge(merge_sort(left), merge_sort(right))
+    merge_sort(l1)
+    merge_sort(l2)
+    merge_sort(l3)
+    merge_sort(l4)
+    merge_sort(l5)
+    merge_sort(l6)
 
+    assertion.equals([1,2,3,4,5], l1)
+    assertion.equals([1,2,3], l2)
+    assertion.equals([3], l3)
+    assertion.equals([1,2,4,8,11,56,777,1234], l4)
+    assertion.equals([2,6], l5)
+    assertion.equals([], l6)
+
+if __name__ == '__main__':
+    main()

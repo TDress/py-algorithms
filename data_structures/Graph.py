953edc6a08f31a776427c31701b678328bf2b429
Thomas Dressler <Thomas.Dressler1@gmail.com>
2018-03-01 19:57:36 -0700

    Test for cycle in a directed graph and get connected components from undirected graphs.

--- data_structures/Graph.py
+++ data_structures/Graph.py
@@ -35,14 +35,67 @@ class NodeAdjacencyMap:
         return hash(id(self))
 
 
-def dfs(node, visited):
-    if not isinstance(node, NodeAdjacencyMap):
-        raise TypeError('argument is not a valid node')
+def explore(node, visited):
     visited.add(node)
     for n in node.children:
         if not n in visited:
-            dfs(n, visited)
+            explore(n, visited)
 
+
+def dfs(G, visited):
+    if not isinstance(G, Graph):
+        raise TypeError('argument is not a valid graph')
+
+    for node in G.nodes:
+        if not node in visited:
+            explore(node, visited)
+
+#These functions for getting connected components of a graph only work
+# for undirected graphs.  Directed is more complicated
+def explore_connected_component(node, visited, ccs, ccs_count):
+    """Modified version of explore function, for counting connected components"""
+    visited.add(node)
+    ccs[node] = ccs_count
+    for c in node.children:
+        if not c in visited:
+            explore_connected_component(c, visited, ccs, ccs_count)
+
+def get_connected_components(G, visited, ccs):
+    """Get the connected components of the graph.  Modified dfs implementation.
+
+    :dict ccs: mapping of nodes to connected component numbers.
+    :int cc_count: Current connected component number
+    """
+    ccs_count = 0
+    for node in G.nodes:
+        if not node in visited:
+            ccs_count += 1
+            explore_connected_component(node, visited, ccs, ccs_count)
+
+    return ccs_count
+
+def is_cyclic_explore(node, visited, path):
+    # previsit
+    path.add(node)
+    visited.add(node)
+
+    for c in node.children:
+        if (c in path 
+            or (not c in visited and is_cyclic_explore(c, visited, path))):
+            # The node has already been seen or a recursive call fails
+            return True
+
+    path.remove(node)
+    return False
+
+def is_cyclic(G, visited):
+    path = set()
+    for node in G.nodes:
+        if not node in visited:
+            if is_cyclic_explore(node, visited, path):
+                return True
+
+    return False
 
 
 def main():
@@ -76,17 +129,39 @@ def main():
     equals(False, nodeG in all_children)
     
     visited = set()
-    dfs(nodeA, visited)
+    explore(nodeA, visited)
     equals(False, nodeG in visited)
     equals(all_children, visited)
     
     visited.clear()
-    dfs(nodeG, visited)
+    explore(nodeG, visited)
     equals(set([nodeG]) | all_children, visited)
 
     visited.clear()
-    dfs(nodeF, visited)
+    explore(nodeF, visited)
     equals(set([nodeE, nodeF]), visited)
+
+    # Find connected components in an undirected graph.  
+    u_graph = Graph()
+    nodeI = NodeAdjacencyMap('I', u_graph)
+    nodeJ = NodeAdjacencyMap('J', u_graph)
+    nodeK = NodeAdjacencyMap('K', u_graph)
+    nodeL = NodeAdjacencyMap('L', u_graph)
+    
+    nodeI.children.add(nodeJ)
+    nodeJ.children.add(nodeI)
+    ccs = {}
+    visited = set()
+
+    ccs_count = get_connected_components(u_graph, visited, ccs)
+    #There should be 3 connected components and one of them should have both I and J
+    equals(3, ccs_count)
+    equals(True, ccs[nodeI] == ccs[nodeJ])
+
+    # Test for cycle in directed graph
+    equals(True, is_cyclic(graph, set()))
+    nodeB.children.clear()
+    equals(False, is_cyclic(graph, set()))
                  
 if __name__ == "__main__":
     main()
